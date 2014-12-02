623ed1b31c8199415fd53e03fe268124ab5a32ec
ken <keniikura@gmail.com>
2014-12-03 00:21:35 +0900

    closedフラグ追加

--- src/main/java/org/devtools/river/GitlabRiver.java
+++ src/main/java/org/devtools/river/GitlabRiver.java
@@ -1,8 +1,10 @@
 package org.devtools.river;
 
+import java.util.Map;
+
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.Requests;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.river.AbstractRiverComponent;
 import org.elasticsearch.river.River;
 import org.elasticsearch.river.RiverName;
@@ -12,6 +14,10 @@ public class GitlabRiver extends AbstractRiverComponent implements River {
     private final Client client;
 
     private GitlabRiverLogic riverLogic;
+
+	private volatile Thread riverThread;
+	
+	private volatile boolean closed;
 
     @Inject
     public GitlabRiver(final RiverName riverName, final RiverSettings settings,
@@ -28,15 +34,20 @@ public class GitlabRiver extends AbstractRiverComponent implements River {
     @Override
     public void start() {
         logger.info("START GitlabRiver");
-
+        Map<String,Object> gitlab = (Map<String,Object>)this.settings.settings().get("gitlab");
+        
         riverLogic = new GitlabRiverLogic();
-        new Thread(riverLogic).start();
+        this.riverThread = EsExecutors.daemonThreadFactory(settings.globalSettings(),
+                "river(" + riverName().getType() + "/" + riverName().getName() + ")")
+                .newThread(riverLogic);
+        this.riverThread.start();
     }
 
     @Override
     public void close() {
         logger.info("CLOSE GitlabRiver");
-
+        this.closed = true;
+        this.riverThread.interrupt();
         // TODO Your code..
     }
 
