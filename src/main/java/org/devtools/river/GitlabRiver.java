f5c866fd042f6fb02480a9a98d23fa10dbd50292
ken <keniikura@gmail.com>
2014-12-04 01:01:29 +0900

    issue noteのjson化とindex追加まで実装（noteのjsonが適当）

--- src/main/java/org/devtools/river/GitlabRiver.java
+++ src/main/java/org/devtools/river/GitlabRiver.java
@@ -1,20 +1,35 @@
 package org.devtools.river;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.river.AbstractRiverComponent;
 import org.elasticsearch.river.River;
 import org.elasticsearch.river.RiverName;
 import org.elasticsearch.river.RiverSettings;
+import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.models.GitlabIssue;
+import org.gitlab.api.models.GitlabNote;
+import org.gitlab.api.models.GitlabProject;
 
 public class GitlabRiver extends AbstractRiverComponent implements River {
 	private final Client client;
@@ -29,13 +44,29 @@ public class GitlabRiver extends AbstractRiverComponent implements River {
 
 	private String indexName;
 
-	private String typeName;
+	// private String typeName;
 
 	private int bulkSize;
 
 	private TimeValue bulkFlushInterval;
 
 	private int maxConcurrentBulk;
+
+	private Pattern projectRegex;
+
+	private int issueMax;
+
+	private int mergeRequestMax;
+
+	private String username;
+
+	private String privateToken;
+
+	private String hostUrl;
+
+	private boolean ignoreCertificateErrors;
+
+	private TimeValue fetchInterval;
 
 	@Inject
 	public GitlabRiver(final RiverName riverName, final RiverSettings settings,
@@ -47,14 +78,41 @@ public class GitlabRiver extends AbstractRiverComponent implements River {
 
 		Map<String, Object> gitlabSettings = (Map<String, Object>) this.settings
 				.settings().get("gitlab");
-		
-        Map<String,Object> indexSettings = (Map<String,Object>)this.settings.settings().get("index");
-        this.indexName = XContentMapValues.nodeStringValue(indexSettings.get("index"), riverName.name());
-        this.typeName = XContentMapValues.nodeStringValue(indexSettings.get("type"), "page");
-        this.bulkSize = XContentMapValues.nodeIntegerValue(indexSettings.get("bulk_size"), 100);
-        this.bulkFlushInterval = TimeValue.parseTimeValue(XContentMapValues.nodeStringValue(
-                indexSettings.get("flush_interval"), "5s"), TimeValue.timeValueSeconds(5));
-        this.maxConcurrentBulk = XContentMapValues.nodeIntegerValue(indexSettings.get("max_concurrent_bulk"), 1);
+
+		this.projectRegex = Pattern.compile(XContentMapValues.nodeStringValue(
+				gitlabSettings.get("project"), riverName.name()));
+		this.issueMax = XContentMapValues.nodeIntegerValue(
+				gitlabSettings.get("issue_max"), -1);
+		this.mergeRequestMax = XContentMapValues.nodeIntegerValue(
+				gitlabSettings.get("merge_request_max"), -1);
+		this.username = XContentMapValues.nodeStringValue(
+				gitlabSettings.get("username"), null);
+		this.privateToken = XContentMapValues.nodeStringValue(
+				gitlabSettings.get("privatetoken"), null);
+		this.hostUrl = XContentMapValues.nodeStringValue(
+				gitlabSettings.get("host_url"), null);
+		this.ignoreCertificateErrors = XContentMapValues.nodeBooleanValue(
+				gitlabSettings.get("ignore_certificate_errors"), false);
+		this.fetchInterval = TimeValue.parseTimeValue(
+				XContentMapValues.nodeStringValue(
+						gitlabSettings.get("fetch_interval"), "120m"),
+						TimeValue.timeValueMinutes(120));
+
+		Map<String, Object> indexSettings = (Map<String, Object>) this.settings
+				.settings().get("index");
+		this.indexName = XContentMapValues.nodeStringValue(
+				indexSettings.get("index"), riverName.name());
+		// this.typeName =
+		// XContentMapValues.nodeStringValue(indexSettings.get("type"),
+		// "issue");
+		this.bulkSize = XContentMapValues.nodeIntegerValue(
+				indexSettings.get("bulk_size"), 100);
+		this.bulkFlushInterval = TimeValue.parseTimeValue(
+				XContentMapValues.nodeStringValue(
+						indexSettings.get("flush_interval"), "5s"),
+				TimeValue.timeValueSeconds(5));
+		this.maxConcurrentBulk = XContentMapValues.nodeIntegerValue(
+				indexSettings.get("max_concurrent_bulk"), 1);
 
 	}
 
@@ -123,26 +181,117 @@ public class GitlabRiver extends AbstractRiverComponent implements River {
 	public void close() {
 		logger.info("CLOSE GitlabRiver");
 		this.closed = true;
-		
+
 		bulkProcessor.close();
 		this.riverThread.interrupt();
 	}
 
 	private class GitlabRiverLogic implements Runnable {
+		private GitlabAPI api;
+
+		private GitlabRiverLogic() {
+			api = GitlabAPI.connect(hostUrl, privateToken);
+			api.ignoreCertificateErrors(ignoreCertificateErrors);
+		}
 
 		@Override
 		public void run() {
 			logger.info("START GitlabRiverLogic: " + client.toString());
 
 			while (!closed) {
+				try {
+					List<GitlabProject> projects = api.getProjects();
+					for (GitlabProject project : projects) {
+						if (projectRegex.matcher(project.getPathWithNamespace())
+								.matches()) {
+							updateIssueIndex(project);
+						}
+					}
+				} catch (Exception e) {
+					logger.warn("GitlabRiverLogic main loop", e);
+				}
 
 				try {
-					Thread.sleep(2000L);
+					logger.info("START GitlabRiverLogic: " + client.toString());
+					Thread.sleep(fetchInterval.millis());
 				} catch (InterruptedException e) {
-					
+
 				}
 			}
 
+		}
+
+		private void updateIssueIndex(GitlabProject project) {
+			logger.info("START GitlabRiverLogic.updateIssueIndex: " + project.getPathWithNamespace());
+			String issuesTail = String.format("%s%s%s", GitlabProject.URL,project.getId(),GitlabIssue.URL);
+			Iterator<GitlabIssue[]> issuesIterator = api.retrieve().asIterator(issuesTail, GitlabIssue[].class);
+			int issueCounter=0;
+			boolean breakFlg = false;
+			while(issuesIterator.hasNext() && !breakFlg){
+				GitlabIssue[] issuesPage =issuesIterator.next();
+				for (GitlabIssue issue : issuesPage) {
+					if(issueMax > 0 && issueMax < issueCounter++) {
+						breakFlg = true;
+						break;
+					}
+					List<GitlabNote> notes = getIssueNotes(issue);
+					
+					try {
+						IndexRequest req = Requests.indexRequest(indexName)
+								.type("issue")
+								.id(String.valueOf(issue.getId()))
+								.source(toIssueJson(project,issue,notes));
+						bulkProcessor.add(req);
+					} catch (IOException e) {
+						logger.warn("issue index", e);
+					}
+				}
+			}
+			
+		}
+
+		private XContentBuilder toIssueJson(GitlabProject project, GitlabIssue issue,
+				List<GitlabNote> notes) throws IOException {
+			XContentBuilder builder = XContentFactory.jsonBuilder();
+			builder.startObject();//issue root start
+			builder.field("id",issue.getId())
+					.field("project_id",issue.getProjectId())
+				.field("project_path",project.getPathWithNamespace())
+				.field("iid",issue.getIid())
+				.field("title",issue.getTitle())
+				.field("description",issue.getDescription())
+				.field("state",issue.getState())
+				.field("author",issue.getAuthor().getUsername())
+				.field("created_at",issue.getCreatedAt())
+				.field("updated_at",issue.getUpdatedAt())
+				.field("assignee",issue.getAssignee().getUsername())
+				.field("milestone",issue.getMilestone().getTitle())
+				.array("labels",issue.getLabels());
+			
+			//add notes
+//			builder.startArray("notes");
+//			for(GitlabNote note : notes){
+//				
+//			}
+//			builder.endArray();
+			ObjectMapper mapper = new ObjectMapper();
+			ObjectWriter writer = mapper.writer();							
+			builder.rawField("notes", writer.writeValueAsBytes(notes));
+			
+			//issue root end
+			builder.endObject();
+			return builder;
+		}
+
+		private List<GitlabNote> getIssueNotes(GitlabIssue issue) {
+			try {
+				return api.getNotes(issue);
+			} catch (IOException e) {
+				logger.error(
+						"faile getIssueNotes() issue.projectId={} issue.iid={}",
+						e, issue.getProjectId(), issue.getIid());
+				return Collections.EMPTY_LIST;
+			}
 		}
 	}
 }
